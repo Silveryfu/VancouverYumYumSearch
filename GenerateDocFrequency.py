456cf1df9932b8cc66f24578638ea90656fcfb34
Silvery Fu <silveryfu@gmail.com>
2013-07-17 00:46:06 +0800

    GDF, Tf-idf, docFrequency, FinalDoc are added, but need to change.

---
+++ GenerateDocFrequency.py
@@ -0,0 +1,35 @@
+import os
+
+def generateDocFrequency():
+    result=[]
+    fileNameList=os.listdir("C:/Users/admin/Desktop/stemmed")
+    fterm=open("C:/Users/admin/SilveryGit/SearchEngine/Dictionary.txt","r")
+    TermLines=fterm.readlines()
+    for term in TermLines:
+        term=term.rstrip("\n")
+        counter=0
+        for i in range(0,len(fileNameList)):
+            f=open("C:/Users/admin/Desktop/stemmed/"+fileNameList[i],"r")
+            lines=f.readlines()
+            flag=1
+            for line in lines:
+                if flag==0:
+                    break
+                line=line.rstrip("\n")  
+                words=line.split()
+                for word in words:
+                    if cmp(term,word)==0:
+                        counter=counter+1
+                        flag=0   #mark the term has been found, and stop searching this file
+                        break
+        result.append([term,counter])
+        print [term,counter]
+    fileResult=open("C:/Users/admin/SilveryGit/SearchEngine/DocumentFrequency.txt","w")
+    for row in result:
+        fileResult.write(str(row[0])+" "+str(row[1])+"\n")
+    print "Process completed."
+def main():
+	generateDocFrequency()
+	
+if __name__ == "__main__":
+	main()
---
+++ Tf-Idf.py
@@ -0,0 +1,36 @@
+import math
+
+#Wtd=(1+logtFtd)log(N/dFt)
+
+def getWeight(term):
+    
+    #calculate the frequency of term in a document 
+    tFtd=0;
+    file=open("Result.txt","r")
+    lines=file.readlines()
+    for line in lines:
+        words=line.split()
+        for word in words:
+            if cmp(word, term)==0:
+                tFtd=tFtd+1
+
+                
+    #calculate the number of documents of term
+    dFt=0
+    file=open("documentFrequency.txt","r")
+    lines=file.readlines()
+    for line in lines:
+        words=line.split()
+        if cmp(words[0],term)==0:
+            dFt=int(words[1])
+    if(tFtd==0):
+        Wtd=0;
+    else:
+        Wtd=(1+math.log10(tFtd))*math.log10(100.0/dFt)
+    file=open("FinalDoc.txt","w")
+    
+def main():
+    print getWeight('obtus')
+
+if __name__=="__main__":
+    main()
