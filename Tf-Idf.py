90174008d4a9f54e5b5925ae3878456741564329
Silvery Fu <silveryfu@gmail.com>
2013-07-17 18:27:13 +0800

    Tf-idf debugged. MatrixGenerator added.

--- Tf-Idf.py
+++ Tf-Idf.py
@@ -2,19 +2,59 @@ import math
 
 #Wtd=(1+logtFtd)log(N/dFt)
 
-def getWeight(term):
+def getWeight(term, docNum):
     
     #calculate the frequency of term in a document 
     tFtd=0;
+    term=term.lower()
     file=open("Document.txt","r")
     lines=file.readlines()
-    for line in lines:
-        words=line.split()
-        for word in words:
-            if cmp(word, term)==0:
-                tFtd=tFtd+1
-
-                
+    NumberOfDoc=len(lines)/10
+    sublines=[]
+    for i in range((docNum-1)*10,(docNum-1)*10+10):
+        sublines.append(lines[i])
+    for i in range(0,10):
+        line=sublines[i].rstrip("\n")
+        if i==1:
+            words=line.split()
+            for word in words:
+                word=word.lower()
+                if term==word:
+                    tFtd=tFtd+1
+        elif i==5:
+            words=line.split()
+            for j in range(len(words)-1):
+                word=words[j].lower()
+                if word==term:
+                    tFtd=tFtd+1
+            words=words[len(words)-1].split(",")
+            for word in words:
+                word=word.lower()
+                if word==term:
+                    tFtd=tFtd+1
+        elif i==6:
+            words=line.split(", ")
+            for word in words:
+                word=word.split()
+                for subword in word:
+                    subword=subword.lower()
+                    if subword==term:
+                        tFtd=tFtd+1
+        elif i==7:
+            words=line.split(", ")
+            for word in words:
+                word=word.split()
+                for subword in word:
+                    subword=subword.lower()
+                    if subword==term:
+                        tFtd=tFtd+1
+        elif i==9:
+            words=line.split()
+            for word in words:
+                word=word.lower()
+                if word==term:
+                    tFtd=tFtd+1
+      
     #calculate the number of documents of term
     dFt=0
     file=open("documentFrequency.txt","r")
@@ -26,14 +66,15 @@ def getWeight(term):
     if(tFtd==0):
         Wtd=0;
     else:
-        Wtd=(1+math.log10(tFtd))*math.log10(100.0/dFt)
-
+        Wtd=(1+math.log10(tFtd))*math.log10(NumberOfDoc/dFt)
+    print NumberOfDoc
     Result=[term,' ', str(Wtd), '\n']  #a line which will be written to the finalDoc   
     fileFinalDoc=open("FinalDoc.txt","a+")
-    fileFinalDoc.writelines(Result)    
+    fileFinalDoc.writelines(Result)
+    print Result
     
 def main():
-    print getWeight('and')
+    print getWeight('robson', 1)
 
 if __name__=="__main__":
     main()
