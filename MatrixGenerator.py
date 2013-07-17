f0b9ff49b683cad28628273defc22485e989b00f
Silvery Fu <silveryfu@gmail.com>
2013-07-17 19:18:57 +0800

    Rename finalMatrix.

--- MatrixGenerator.py
+++ MatrixGenerator.py
@@ -7,11 +7,20 @@ def generateMatrix():
     dicLines=fDic.readlines()
     NumberOfDoc=len(docLines)/10
     Matrix=[]
-    for term in dicLines:
+    for line in dicLines:
         term=line.rstrip("\n")
         termVector=[term,","]
         for i in range(0, NumberOfDoc):
             Wtd=getWeight(term, i+1)
-            termVector.append(str(i+1)+" "+str(Wtd)+",")
-            print termVector
-        
+            if Wtd==0:
+                continue
+            else:
+                termVector.append(str(i+1)+" "+str(Wtd)+",")
+        print termVector
+
+
+def main():
+    generateMatrix()
+
+if __name__=="__main__":
+    main()
