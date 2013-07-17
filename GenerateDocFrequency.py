6ca088884a566ebc6230f80bc874dcb8fea9cabd
Silvery Fu <silveryfu@gmail.com>
2013-07-17 15:08:03 +0800

    GenreateDocFrequency completed.

--- GenerateDocFrequency.py
+++ GenerateDocFrequency.py
@@ -2,31 +2,86 @@ import os
 
 def generateDocFrequency():
     result=[]
-    fileNameList=os.listdir("C:/Users/admin/Desktop/stemmed")
-    fterm=open("Dictionary.txt","r")
-    TermLines=fterm.readlines()
-    for term in TermLines:
+    fDoc=open("Document.txt","r")
+    fTerm=open("Dictionary.txt","r")
+    docLines=fDoc.readlines()
+    termLines=fTerm.readlines()
+    docTotalLength=len(docLines)
+    for term in termLines:
         term=term.rstrip("\n")
         counter=0
-        for i in range(0,len(fileNameList)):
-            f=open("C:/Users/admin/Desktop/stemmed/"+fileNameList[i],"r")
-            lines=f.readlines()
+        docNum=0              
+        for docNum in range(0,docTotalLength/10):
             flag=1
-            for line in lines:
+            for i in range(0,10):
                 if flag==0:
                     break
-                line=line.rstrip("\n")  
-                words=line.split()
-                for word in words:
-                    if cmp(term,word)==0:
-                        counter=counter+1
-                        flag=0   #mark the term has been found, and stop searching this file
-                        break
-        result.append([term,counter])
+                else:
+                    line=docLines[docNum*10+i].rstrip("\n")
+                    if i==1:
+                        print line
+                        words=line.split()
+                        for word in words:
+                            word = word.lower()
+                            if word==term:
+                                counter=counter+1
+                                flag=0
+                                break
+                    elif i==5:
+                        words=line.split()
+                        print line
+                        for j in range(len(words)-1):
+                            word=words[j].lower()
+                            if word==term:
+                                counter=counter+1
+                                flag=0
+                                break
+                        if flag==0:
+                            break
+                        words=words[len(words)-1].split(",")
+                        for word in words:
+                            word=word.lower()
+                            if word==term:
+                                counter=counter+1
+                                flag=0
+                                break
+                    elif i==6:
+                        words=line.split(", ")
+                        for word in words:
+                            word=word.split()
+                            for subword in word:
+                                subword=subword.lower()
+                                if subword==term:
+                                    counter=counter+1
+                                    flag=0
+                                    break
+                    elif i==7:
+                        words=line.split(", ")
+                        for word in words:
+                            word=word.split()
+                            for subword in word:
+                                subword=subword.lower()
+                                if subword==term:
+                                    counter=counter+1
+                                    flag=0
+                                    break
+                    elif i==9:
+                        words=line.split()
+                        for word in words:
+                            if word==term:
+                                counter=counter+1
+                                flag=0
+                                break
+                    else:
+                        continue
+                        
+        result.append([term, counter])
         print [term,counter]
-    fileResult=open("C:/Users/admin/SilveryGit/SearchEngine/documentFrequency.txt","w")
+
+    fileResult=open("documentFrequency.txt","a+")
     for row in result:
         fileResult.write(str(row[0])+" "+str(row[1])+"\n")
+        
     print "Process completed."
 def main():
 	generateDocFrequency()
